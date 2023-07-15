484f582f3ff0e5d60edc5e4d543cf4be5f579e1c
Hasibur Rahman <hasiburdev@gmail.com>
2023-07-15 21:50:36 +0600

    Add edit feature

--- src/components/edit-link.tsx
+++ src/components/edit-link.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 import { Button } from "@/components/ui/button";
 import {
@@ -11,37 +12,112 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ShortLink, ShortLinks } from "@/interfaces/short-link";
+import { BASE_URL } from "@/lib/constants";
 import { Edit } from "lucide-react";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
+import { getAllLinks, updateLink } from "@/lib/local-storage";
 
-export const EditLink = () => {
+interface EditLinkProps {
+  link: ShortLink;
+  setLinks: Dispatch<SetStateAction<ShortLinks>>;
+}
+
+export const EditLink = ({ setLinks, link }: EditLinkProps) => {
+  const targetRef = useRef<HTMLButtonElement>(null);
+  const [url, setUrl] = useState(link.url);
+  const [existingShortCode, setExistingShortCode] = useState(false);
+  const [invalidUrl, setInvalidUrl] = useState(false);
+  const [shortUrl, setShortUrl] = useState(link.shortId);
+
+  const closeDialog = () => {
+    targetRef.current?.click();
+  };
+
+  const handleUpdate = () => {
+    if (shortUrl === "") return;
+    try {
+      const validUrl = new URL(url);
+      const exist = updateLink(link.shortId, shortUrl, validUrl.href);
+      if (exist === true) {
+        setExistingShortCode(true);
+      } else {
+        setLinks(getAllLinks());
+        setExistingShortCode(false);
+        closeDialog();
+      }
+    } catch (error) {
+      setInvalidUrl(true);
+    }
+  };
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Edit className="h-4 w-4 cursor-pointer stroke-blue-300" />
+      <DialogTrigger ref={targetRef} asChild>
+        <button>
+          <Edit className="h-4 w-4 cursor-pointer stroke-blue-300" />
+        </button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[80vw] lg:max-w-[60vw]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Edit Url</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you@apos;re done.
+            Don't forget to put{" "}
+            <code className="py-0.5 px-1 rounded-sm bg-muted">https://</code> or{" "}
+            <code className="py-0.5 px-1 rounded-sm bg-muted">http://</code> in
+            url!
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
+            <Label htmlFor="shortUrl" className="text-right">
+              Short Url
             </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            <Input
+              id="shortUrl"
+              value={shortUrl}
+              onChange={(e) => {
+                setShortUrl(e.target.value);
+                if (existingShortCode) setExistingShortCode(false);
+              }}
+              placeholder={`${BASE_URL}/abcd`}
+              className="col-span-3"
+            />
+            {existingShortCode && (
+              <p className="text-red-400 italic -mt-1 col-start-2 text-xs row-start-2 col-span-2">
+                This code is already in use!
+              </p>
+            )}
           </div>
+
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
+            <Label htmlFor="url" className="text-right">
+              URL
             </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Input
+              id="rul"
+              value={url}
+              onChange={(e) => {
+                setUrl(e.target.value);
+                if (invalidUrl) setInvalidUrl(false);
+              }}
+              placeholder="https://github.com/hasiburdev/url-shortener"
+              className="col-span-3"
+            />{" "}
+            {invalidUrl && (
+              <p className="text-red-400 italic -mt-1 col-start-2 text-xs row-start-2 col-span-2">
+                This url is not valid!
+              </p>
+            )}
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button variant="outline" onClick={closeDialog}>
+            Cancel
+          </Button>
+          <Button onClick={handleUpdate} disabled={shortUrl.length === 0}>
+            Save changes
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
--- src/components/links.tsx
+++ src/components/links.tsx
@@ -29,30 +29,32 @@ export const Links = () => {
         <div className="">Actions</div>
       </div>
       <div className="divide-y-2 divide-muted overflow-auto">
-        {Object.values(links).map((link, index) => (
-          <div
-            key={link.shortId}
-            className="grid grid-cols-[1fr_2fr_5fr_1fr_1fr] w-full p-3"
-          >
-            <div>{index + 1}</div>
-            <div className="font-medium">{link.shortId}</div>
-            <div className="truncate pr-4">
-              <a
-                className="underline text-slate-500 pr-3"
-                href={link.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {link.url}
-              </a>
+        {Object.values(links)
+          .sort((a, b) => (a.updatedAt < b.updatedAt ? 1 : -1))
+          .map((link, index) => (
+            <div
+              key={link.shortId}
+              className="grid grid-cols-[1fr_2fr_5fr_1fr_1fr] w-full p-3"
+            >
+              <div>{index + 1}</div>
+              <div className="font-medium">{link.shortId}</div>
+              <div className="truncate pr-4">
+                <a
+                  className="underline text-slate-500 pr-3"
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {link.url}
+                </a>
+              </div>
+              <div className="text-center">{link.visits}</div>
+              <div className="text-right flex items-center justify-end gap-2">
+                <EditLink link={link} setLinks={setLinks} />
+                <DeleteLink setLinks={setLinks} shortId={link.shortId} />
+              </div>
             </div>
-            <div className="text-center">{link.visits}</div>
-            <div className="text-right flex items-center justify-end gap-2">
-              <EditLink />
-              <DeleteLink setLinks={setLinks} shortId={link.shortId} />
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
