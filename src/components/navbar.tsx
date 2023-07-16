d27bfb8b2f05fa236c9f6d4f073a5316bc80abbd
Hasibur Rahman <hasiburdev@gmail.com>
2023-07-17 01:55:56 +0600

    Added dark mode

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,30 @@
 "use client";
 import Link from "next/link";
 import { Separator } from "./ui/separator";
+
+import { useTheme } from "next-themes";
+import { Label } from "./ui/label";
+import { Switch } from "./ui/switch";
+import { useEffect, useState } from "react";
+
 export const Navbar = () => {
+  const { setTheme, theme } = useTheme();
+  const [checked, setChecked] = useState(false);
+  console.log(theme);
+
+  useEffect(() => {
+    setChecked(theme === "dark");
+  }, []);
+
+  const changeTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+      setChecked(false);
+    } else {
+      setTheme("dark");
+      setChecked(true);
+    }
+  };
   return (
     <div className="w-full ">
       <nav className="flex items-center justify-between mb-2">
@@ -16,7 +39,14 @@ export const Navbar = () => {
             All Links
           </Link>
         </div>
-        <div>Dark Mode</div>
+        <div className="flex items-center space-x-2">
+          <Label htmlFor="dark-mode">Dark Mode</Label>
+          <Switch
+            id="dark-mode"
+            checked={checked}
+            onCheckedChange={changeTheme}
+          />
+        </div>
       </nav>
       <Separator />
     </div>
